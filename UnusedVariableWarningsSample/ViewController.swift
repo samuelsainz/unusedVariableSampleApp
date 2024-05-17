f0ce2bfcde13ee7175fe26d13bbf7a564a2093a9
Samuel Sainz <nsamusan@amazon.com>
2024-05-17 15:11:02 -0300

    Initial Commit

---
+++ UnusedVariableWarningsSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnusedVariableWarningsSample
+//
+//  Created by Sainz Sanchez, Samuel [C] on 17/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
